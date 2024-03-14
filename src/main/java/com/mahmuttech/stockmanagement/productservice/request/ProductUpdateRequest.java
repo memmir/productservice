1e8414173445904015b1225ef22e93134c153a25
memmir <mahmutekilinc@gmail.com>
2024-03-14 14:28:50 +0300

    Created Request classes.

---
+++ src/main/java/com/mahmuttech/stockmanagement/productservice/request/ProductUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.mahmuttech.stockmanagement.productservice.request;
+
+import lombok.Data;
+
+@Data
+public class ProductUpdateRequest {
+    private long productId;
+    private String productName;
+    private Integer quantity;
+    private Double price;
+
+}
