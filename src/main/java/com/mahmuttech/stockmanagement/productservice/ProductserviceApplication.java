ad341a0fe11227a8ea142f3b09e961c41414f93a
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-03-21 00:22:33 +0300

    removed initial security

--- src/main/java/com/mahmuttech/stockmanagement/productservice/ProductserviceApplication.java
+++ src/main/java/com/mahmuttech/stockmanagement/productservice/ProductserviceApplication.java
@@ -2,8 +2,9 @@ package com.mahmuttech.stockmanagement.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
