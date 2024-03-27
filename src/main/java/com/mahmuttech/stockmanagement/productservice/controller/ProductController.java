9f30a477855b589b5dc5991facb9397f0bd242e2
memmir <mahmutekilinc@gmail.com>
2024-03-27 14:56:13 +0300

    Created updateProduct endpoint.

--- src/main/java/com/mahmuttech/stockmanagement/productservice/controller/ProductController.java
+++ src/main/java/com/mahmuttech/stockmanagement/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.mahmuttech.stockmanagement.productservice.exception.enums.FriendlyMes
 import com.mahmuttech.stockmanagement.productservice.exception.utils.FriendlyMessageUtils;
 import com.mahmuttech.stockmanagement.productservice.repository.entity.Product;
 import com.mahmuttech.stockmanagement.productservice.request.ProductCreateRequest;
+import com.mahmuttech.stockmanagement.productservice.request.ProductUpdateRequest;
 import com.mahmuttech.stockmanagement.productservice.response.FriendlyMessage;
 import com.mahmuttech.stockmanagement.productservice.response.InternalApiResponse;
 import com.mahmuttech.stockmanagement.productservice.response.ProductResponse;
@@ -50,8 +51,29 @@ public class ProductController {
         log.debug("[{}][getProduct] -> request: {}", this.getClass().getSimpleName(), productId);
         Product product = iProductRepositoryService.getProduct(language,productId);
         ProductResponse productResponse = convertProductResponse(product);
-        log.debug("[{}][getProduct] -> response: {}", this.getClass().getSimpleName(), productId);
+        log.debug("[{}][getProduct] -> response: {}", this.getClass().getSimpleName(), productResponse);
         return InternalApiResponse.<ProductResponse>builder()
+                .httpStatus(HttpStatus.OK)
+                .hasError(false)
+                .payload(productResponse)
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/{language}/update/{productId}")
+    public InternalApiResponse<ProductResponse> updateProduct(@PathVariable("language") Language language,
+                                                              @PathVariable("productId") Long productId,
+                                                              @RequestBody ProductUpdateRequest productUpdateRequest){
+        log.debug("[{}][updateProduct] -> request: {}", this.getClass().getSimpleName(),productId, productUpdateRequest);
+        Product product= iProductRepositoryService.updateProduct(language, productId,productUpdateRequest);
+        ProductResponse productResponse = convertProductResponse(product);
+        log.debug("[{}][updateProduct] -> request: {}", this.getClass().getSimpleName(), productResponse);
+
+        return InternalApiResponse.<ProductResponse>builder()
+                .friendlyMessage(FriendlyMessage.builder()
+                        .title(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.SUCCES))
+                        .description(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.PRODUCT_SUCCESFULLY_UPDATED))
+                        .build())
                 .httpStatus(HttpStatus.OK)
                 .hasError(false)
                 .payload(productResponse)
