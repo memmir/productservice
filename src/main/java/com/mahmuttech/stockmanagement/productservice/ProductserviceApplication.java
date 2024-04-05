15985aa5f3951302d5ef5e252087464e6fd16846
memmir <mahmutekilinc@gmail.com>
2024-04-05 16:01:59 +0300

    Added Eureka server dependencies to ProductserviceApplication.

--- src/main/java/com/mahmuttech/stockmanagement/productservice/ProductserviceApplication.java
+++ src/main/java/com/mahmuttech/stockmanagement/productservice/ProductserviceApplication.java
@@ -3,8 +3,10 @@ package com.mahmuttech.stockmanagement.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableDiscoveryClient //bu anotasyon bir spring boot uygulamasının bir eureka server a bağlanıp servislerinin görünür hale gelmesini sağlar.
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
