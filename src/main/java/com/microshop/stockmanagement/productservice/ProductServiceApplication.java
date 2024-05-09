26da162e6d8f9bec25105d2eb1132fa5be849338
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-09 17:40:15 +0300

    Added feign client

--- src/main/java/com/microshop/stockmanagement/productservice/ProductServiceApplication.java
+++ src/main/java/com/microshop/stockmanagement/productservice/ProductServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableDiscoveryClient //bu anotasyon bir spring boot uygulamasının bir eureka server a bağlanıp servislerinin görünür hale gelmesini sağlar.
 public class ProductServiceApplication {
